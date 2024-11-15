7372325af66c702f5fc9de195486d4866d21d9a4
Parth5409 <parthgaikwad5409@gmail.com>
2024-11-15 14:20:17 +0530

    Added backend URL

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8080/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8080/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8080/api/v1/company";
-export const ROADMAP_API_END_POINT="http://localhost:8080/api/v1/roadmap";
+export const BASE_API_URL = process.env.REACT_APP_API_URL;
+
+// Updated endpoints
+export const USER_API_END_POINT = `${BASE_API_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_API_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_API_URL}/api/v1/company`;
+export const ROADMAP_API_END_POINT = `${BASE_API_URL}/api/v1/roadmap`;
