a925ea3a0d2a5534166d3ab1d3633c2aa4ff12d3
Parth5409 <parthgaikwad5409@gmail.com>
2024-11-15 18:17:26 +0530

    Vercel setup

--- Backend/index.js
+++ Backend/index.js
@@ -16,11 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "https://seeker-scope.vercel.app", // Include deployed frontend URL
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: process.env.CLIENT_URL || "https://seeker-scope.vercel.app", // Allow your frontend
+//   credentials: true,
+// };
+// app.use(cors(corsOptions)); // CORS applied here
 
 // API's
 app.use("/api/v1/user", userRoute);
