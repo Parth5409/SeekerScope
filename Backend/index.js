aa3fac6f9e7e2974b07b0125e664dc08622e9928
Parth5409 <parthgaikwad5409@gmail.com>
2024-11-15 14:57:11 +0530

    Vercel setup

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // Include deployed frontend URL
   credentials: true,
 };
 app.use(cors(corsOptions));
